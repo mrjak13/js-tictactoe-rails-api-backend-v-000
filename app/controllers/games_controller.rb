9e649896376cf00ec99fd17b6af7e2f3812448ed
mrjak13 <justinkenney13@gmail.com>
2019-04-22 11:32:43 -0400

    set up routes and actions for game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,19 @@
 class GamesController < ApplicationController
   # Add your GamesController code here
+  def index
+  end
+
+  def show
+  	@game = Game.find(params[:id])
+  	render json: @game, status: 200
+  end
+
+  def create
+  	@game = Game.new
+  	render json: @game, status: 201
+  end
+
+  def update
+  end
 
 end
