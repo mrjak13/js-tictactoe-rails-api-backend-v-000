11a22b7d9eef416b9ff1285251205674b7dc7f4d
mrjak13 <justinkenney13@gmail.com>
2019-04-22 12:35:39 -0400

    update model to serialize arrays

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,19 +1,31 @@
 class GamesController < ApplicationController
   # Add your GamesController code here
   def index
+  	@games = Game.all
+  	render json: @games
   end
 
   def show
   	@game = Game.find(params[:id])
-  	render json: @game, status: 200
+  	render json: @game
   end
 
   def create
+  	
   	@game = Game.new
-  	render json: @game, status: 201
+  	@game.state = params[:state]
+  	@game.save
+
+  	render json: @game
   end
 
   def update
+
+  	@game = Game.find(params[:id])
+  	@game.update(state: params[:state])
+
+  	render json: @game 	
+
   end
 
 end
